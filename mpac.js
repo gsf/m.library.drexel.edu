ead0cb474014e7225c26a6d9bf99ee2c99d751d2
Gabriel Farrell <gsf747@gmail.com>
2010-10-15 17:48:13 -0400

    Added results template and more parsing

--- mpac.js
+++ mpac.js
@@ -1,8 +1,11 @@
+var fs = require('fs');
 var htmlparser = require('htmlparser');
 var http = require('http');
+//var querystring = require('querystring');
 var select = require('soupselect').select;
 var sys = require('sys');
 var url = require('url');
+var ejs = require('ejs');
 
 var innoserv = http.createClient(80, 'innoserv.library.drexel.edu');
 
@@ -10,8 +13,10 @@ var server = http.createServer(function (request, response) {
   console.log('Request from ' + request.url);
   var url_parts = url.parse(request.url);
   if (url_parts.pathname ==='/') {
-    // url_parts.query should be searcharg=blah+blah
-    var search_path = '/search~S9/?searchtype=X&' + url_parts.query;
+    // url_parts.query should be the search terms
+    var query = url_parts.query || '';
+    console.log('Query: ' + query);
+    var search_path = '/search~S9?/X(' + query + ')';
     console.log(search_path);
     var search_request = innoserv.request(
       'GET', search_path,
@@ -19,22 +24,38 @@ var server = http.createServer(function (request, response) {
       , 'connection': 'keep-alive'
     });
     search_request.end();
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, {'Content-Type': 'text/html'});
     search_request.on('response', function (search_response) {
       //response.write('STATUS: ' + search_response.statusCode + '\n');
       //response.write('HEADERS: ' + JSON.stringify(search_response.headers) + '\n\n');
       var handler = new htmlparser.DefaultHandler(function (error, dom) {
-        if (error) {
-          response.end(error);
-        } else {
-          var resultCount = select(dom, 'div.browseSearchtoolMessage i')[0];
-          if (resultCount) {
-            response.end(resultCount.children[0].data);
-          } else {
-            response.end('0 results found.');
+        if (error) response.end(error);
+        fs.readFile('results.html', function (error, data) {
+          if (error) response.end(error);
+          var context = {locals: {
+            query: query,
+            count: '0 records found.',
+            bibs: []
+          }};
+          var count = select(dom, 'div.browseSearchtoolMessage i')[0];
+          console.log('Count: ' + count);
+          if (count) {
+            context.locals.count = count.children[0].data;
+            var titles = select(dom, 'span.briefcitTitle a');
+            var actions = select(dom, 'div.briefcitActions');
+            var items = select(dom, 'div.briefcitItems');
+            context.locals.bibs = [];
+            for (var i=0; i < titles.length; i++) {
+              bib = {}
+              bib.title = titles[i].children[0].data;
+              bib.items = []; // TODO: grab actions or items
+              context.locals.bibs.push(bib)
+              //response.write(titles[i].children[0].data + '\n');
+            }
           }
-          //response.end(JSON.stringify(dom));
-        }
+          console.log('Context: ' + sys.inspect(context));
+          response.end(ejs.render(data.toString('utf8'), context));
+        });
       }
       , {verbose: false, ignoreWhitespace: true}
       );
