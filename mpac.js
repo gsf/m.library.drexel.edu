8d269c33cb6cda6d3c038e3da6c265c2a35a9602
Gabriel Farrell <gsf747@gmail.com>
2010-10-19 20:31:07 -0400

    Paging and numbers!

--- mpac.js
+++ mpac.js
@@ -11,58 +11,81 @@ var innoserv = http.createClient(80, 'innoserv.library.drexel.edu');
 
 var server = http.createServer(function (request, response) {
   console.log('Request from ' + request.url);
-  var url_parts = url.parse(request.url, true);
-  if (url_parts.pathname ==='/') {
-    var query = url_parts.query.q || '';
+  var urlParts = url.parse(request.url, true);
+  if (urlParts.pathname ==='/') {
+    var query = urlParts.query.q || '';
+    var queryEscaped = querystring.escape(query);
     console.log('Query: ' + query);
-    var search_path = '/search~S9?/X(' + querystring.escape(query) + ')';
-    console.log(search_path);
-    var search_request = innoserv.request(
-      'GET', search_path,
+    // range is grabbed from innopac, looks like "51,1036,1036"
+    var range = urlParts.query.r || '';
+    var rangeEscaped = querystring.escape(range);
+    var start = 1;
+    var searchPath = '/search~S9?/X(' + queryEscaped + ')';
+    if (range) {
+      // start is the digits in range up to the first comma
+      start = range.match(/\d+(?=,)/)[0];
+      console.log(start);
+      searchPath = '/search~S9?/X(' + queryEscaped + ')&/X(' + queryEscaped + ')&/' + rangeEscaped + '/browse';
+    } 
+    console.log(searchPath);
+    var searchRequest = innoserv.request(
+      'GET', searchPath,
       { 'host': 'innoserv.library.drexel.edu' 
       , 'connection': 'keep-alive'
     });
-    search_request.end();
+    searchRequest.end();
     response.writeHead(200, {'Content-Type': 'text/html'});
-    search_request.on('response', function (search_response) {
-      //response.write('STATUS: ' + search_response.statusCode + '\n');
-      //response.write('HEADERS: ' + JSON.stringify(search_response.headers) + '\n\n');
+    searchRequest.on('response', function (searchResponse) {
+      //response.write('STATUS: ' + searchResponse.statusCode + '\n');
+      //response.write('HEADERS: ' + JSON.stringify(searchResponse.headers) + '\n\n');
       var handler = new htmlparser.DefaultHandler(function (error, dom) {
         if (error) response.end(error);
         fs.readFile('search.html', function (error, data) {
           if (error) response.end(error);
-          var context = {locals: {
+          var locals = {
             query: query,
-            count: '0 records found.',
-            bibs: []
-          }};
-          var count = select(dom, 'div.browseSearchtoolMessage i')[0];
+            queryEscaped: queryEscaped,
+            count: 'No records found.',
+            prev: '',
+            next: '',
+            start: start,
+            // TODO titles needs to be a list in case two
+            // titles are the same -- for (x in list) { list[x] }
+            titles: {}
+          };
+          var count = select(dom, 'td.browseHeaderData')[0];
           //console.log('Count: ' + count);
           if (count) {
-            context.locals.count = count.children[0].data;
+            locals.count = count.children[0].data.match(/\((.+)\)/)[1];
+            var pages = select(dom, 'td.browsePager a');
+            var rangeRe = /\/([^\/]+)%2CB\/browse/;
+            pages.forEach(function (p) {
+              if (p.children[0].data === 'Prev') {
+                locals.prev = p.attribs.href.match(rangeRe)[1];
+              } else if (p.children[0].data === 'Next') {
+                locals.next = p.attribs.href.match(rangeRe)[1];
+              }
+            });
             var titles = select(dom, 'span.briefcitTitle a');
             var actions = select(dom, 'div.briefcitActions');
             var items = select(dom, 'div.briefcitItems');
-            context.locals.bibs = [];
             for (var i=0; i < titles.length; i++) {
-              bib = {}
-              bib.title = titles[i].children[0].data;
-              bib.items = []; // TODO: grab actions or items
-              context.locals.bibs.push(bib)
-              //response.write(titles[i].children[0].data + '\n');
+              var title = titles[i].children[0].data;
+              var items = []; // TODO: grab actions or items
+              locals.titles[title] = items;
             }
           }
-          //console.log('Context: ' + sys.inspect(context));
-          response.end(ejs.render(data.toString('utf8'), context));
+          //console.log('Context: ' + sys.inspect(locals));
+          response.end(ejs.render(data.toString('utf8'), {locals: locals}));
         });
       }
       , {verbose: false, ignoreWhitespace: true}
       );
       var parser = new htmlparser.Parser(handler);
-      search_response.on('data', function (chunk) {
+      searchResponse.on('data', function (chunk) {
         parser.parseChunk(chunk);
       });
-      search_response.on('end', function () {
+      searchResponse.on('end', function () {
         parser.done();
       });
     });
